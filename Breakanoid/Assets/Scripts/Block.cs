db1cf5e3ccbc415701610b4f7945ffbb8e651122
kjwasik <krzysztofwasik1988@wp.pl>
2020-03-17 22:47:20 +0000

    Refactoring of the code. New assets and auto play function added.

--- Breakanoid/Assets/Scripts/Block.cs
+++ Breakanoid/Assets/Scripts/Block.cs
@@ -8,7 +8,6 @@ public class Block : MonoBehaviour
     // Configuration parameters
     [SerializeField] AudioClip blockBreakSound;
     [SerializeField] GameObject blockSparklesVFX;
-    [SerializeField] int maxHits;
     [SerializeField] Sprite[] hitSprites;
 
     // Cached reference
@@ -42,6 +41,7 @@ public class Block : MonoBehaviour
     private void HandleHit()
     {
         timesHit++;
+        int maxHits = hitSprites.Length + 1;
 
         if (timesHit >= maxHits)
         {
@@ -55,7 +55,15 @@ public class Block : MonoBehaviour
 
     private void ShowNextHitSprite()
     {
-        GetComponent<SpriteRenderer>().sprite = hitSprites[timesHit - 1];
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array. " + gameObject.name);
+        }
     }
 
     private void DestroyBlock()
--- Breakanoid/Assets/Scripts/Paddle.cs
+++ Breakanoid/Assets/Scripts/Paddle.cs
@@ -8,19 +8,35 @@ public class Paddle : MonoBehaviour
     [SerializeField] float minX = 1f;
     [SerializeField] float maxX = 15f;
 
+    // Cached reference
+    GameSession myGameSession;
+    Ball myBall;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myGameSession = FindObjectOfType<GameSession>();
+        myBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Movement of Paddle in X position
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPosition(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPosition()
+    {
+        if (myGameSession.IsAutoPlayEnabled())
+        {
+            return myBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
